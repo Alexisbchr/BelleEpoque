21c847918489b1087d04506d8f1dc7e4401065b0
alexis <alexis>
2022-09-05 10:28:15 +0000

    work on dashboard responsive

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -132,5 +132,40 @@ class AdminController
       exit;
     }
   }
+  public function ordercours()
+  {
+    $page = "ordercours";
+    $pageName = "Trier les cours";
+    require_once './src/managers/CoursManager.php';
+    $coursManager = new CoursManager;
+    $orderCoursByLetter = $coursManager->orderCoursByLetter();
     
+    //Tri des cours dans une table adaptée (1ère lettre = clé)
+    $coursByFirstLetter = [];
+    $previousFirstLetter = null;
+    $j = 0;
+    for($i = 0, $max = count($orderCoursByLetter); $i < $max; $i++){
+      $orderCoursByLetter[$i]['title']." Premier Char : ".strtoupper(substr($orderCoursByLetter[$i]['title'],0,1));
+      $title = $orderCoursByLetter[$i]['title'];
+      $id = $orderCoursByLetter[$i]['id'];
+      $firstLetter = strtoupper(substr($orderCoursByLetter[$i]['title'],0,1));
+      if($firstLetter !== $previousFirstLetter){
+        $j = 0;
+      }
+      $coursByFirstLetter[$firstLetter][$j]['title'] = $title;
+      $coursByFirstLetter[$firstLetter][$j]['id'] = $id;
+      $previousFirstLetter = $firstLetter;
+      $j++;
+    }
+    return $coursByFirstLetter;
+  }
+  public function orderbyletter(string $letter)
+  {
+    $page = "ordercoursbyletter";
+    $pageName = "Triage cours";
+    require_once './src/managers/CoursManager.php';
+    $coursByFirstLetter = $this->ordercours();
+    require './src/templates/admin/admlayout.phtml';
+  }
+  
 }
